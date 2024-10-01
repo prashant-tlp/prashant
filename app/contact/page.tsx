57d63715995162db7fc7b805eb7bce126c0f985a
prashant-tlp <prashantsinghrajput56@gmail.com>
2024-10-01 07:55:54 +0530

    removed form event

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import axios from 'axios'
 import { enqueueSnackbar, SnackbarProvider } from 'notistack';
 interface ServerResponse {
