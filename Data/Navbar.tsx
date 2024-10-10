5dc2e306d9b10f87b154ba8fed281d16128f5e22
Prashant Singh <prashantsinghrajput56@gmail.com>
2024-10-10 10:43:35 +0530

    fixed

--- Data/Navbar.tsx
+++ Data/Navbar.tsx
@@ -2,6 +2,6 @@ export const NavData: any = [
     // {name:'HOME',type:'text', Link:'/'},
     {name:'ABOUT',type:'text', Link:'/about'},
     {name:'SKILL',type:'text', Link:'/skills'},
-    {name:'PROJECT',type:'text', Link:'/projects'},
+    // {name:'PROJECT',type:'text', Link:'/projects'},
     {name:'CONTACT',type:'text', Link:'/contact'}
 ]
