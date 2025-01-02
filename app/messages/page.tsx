e9aa31730abe44bf081493c7c0f3a8e33ddf9ca1
prashant <prashantsinghrajput56@gmail.com>
2025-01-02 13:43:49 +0530

    `Refactor messages page layout and update resume PDF`

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -28,13 +28,25 @@ const Page = () => {
   return(
   <div className="bg-red-300 w-full h-full p-2">
     {message.map((item:result, index:number) => (
-        <ul key={index} className="flex flex-1 gap-5 m-2 bg-green-300 text-left">
-            <li className="text-left " >{index+1}</li>
-            <li className="text-left w-3/12">{moment(item.date).format("DD/MM/YYYY hh:mm A")}</li>
-            <li className="text-left w-3/12">{item.name}</li>
-            <li className="text-left w-3/12">{item.email}</li>
-            <li className="text-left w-3/12">{item.message}</li>
-        </ul>
+      <ul
+  key={index}
+  className="flex flex-wrap items-center gap-5 p-4 bg-green-200 rounded-lg shadow-md hover:bg-green-300 transition-all"
+>
+  <li className="text-left font-bold w-1/12">{index + 1}</li>
+  <li className="text-left w-2/12 text-sm font-medium text-gray-700">
+    {moment(item.date).format("DD/MM/YYYY hh:mm A")}
+  </li>
+  <li className="text-left w-2/12 text-sm font-medium text-gray-700 truncate">
+    {item.name}
+  </li>
+  <li className="text-left w-3/12 text-sm font-medium text-gray-700 truncate">
+    {item.email}
+  </li>
+  <li className="text-left w-4/12 text-sm font-medium text-gray-700 truncate">
+    {item.message}
+  </li>
+</ul>
+
         ))}
 
   </div>
