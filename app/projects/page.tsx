6538b2e18682faa3397290cf0590a621d93ea38a
prashant-tlp <prashantsinghrajput56@gmail.com>
2025-01-11 17:32:54 +0530

    `Added lazy loading to project images and adjusted image sizes for responsive design`

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,7 +14,8 @@ const page = () => {
       <div>
         {projects.map((project, index) => (
           <div key={index} className=" p-4 m-4 rounded-md shadow flex-col sm:flex-row flex flex-grow gap-5 ">
-            <img src={project.image} alt="dfgh" className="h-1/5 w-2/5 " />
+            <img src={project.image} alt="dfgh" loading="lazy" className="h-full w-4/5 p-0.5 sm:p-0 sm:h-1/5 sm:w-2/5 " />
+
             <span>
               <h2 className="text-2xl flex gap-10 items-center">{project.name}<Link href={project.pLink} className="text-green-500" target="_blank"><FaExternalLinkAlt /></Link></h2>
               <p>{project.description}</p>
